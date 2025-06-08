d23cf4ae8034717af94442de732e5a35dfc89ddc
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2025-06-08 18:55:59 -0500

    Removed test code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,6 @@
 function setup() {
   noCanvas();
-
   formatter = new Formatter();
-
-  // Testing
-  // setInputText("select 1 as num, 'test' as test from table t where (t.id = 1 and 1 and t.count = 0) and TRUE and 1 or FALSE and TRUE");
-  // setInputText("select count(*) from table1 cross join table2;");
-  setInputText(`with
-    
-    test as (select  
-  1 as num, 'test'    as test
-from 
-table t 
-inner   
-join table2 t2 on      t1.id=t2.id
-where   
-
-
- (t.id = 1 
-and 1 and t.name in ('test', 'test2', 'test3', 'test4') and
-
-t.count.count(1) =  0
-
-and true and(false or false and 1> 0 or true and (not false)))
-and TRUE and 1 or FALSE and
-
-
-
-    TRUE
-having count(*) > 0), foo as (select * from test)
-select * from foo;`);
-  // setInputText("select * from table;");
-  formatCode();
 }
 
 function getInputText() {
